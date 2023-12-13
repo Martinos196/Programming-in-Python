161c14c18f56947573b7ef97278d879b1a0f2a57
Martinos196 <giskamarcin@gmail.com>
2023-12-13 01:35:07 +0100

    Ostatnie zmiany

--- Zad2/chase/main.py
+++ Zad2/chase/main.py
@@ -4,33 +4,33 @@ import json
 import csv
 
 class Sheep:
-    def __init__(self, sheep_move, init_pos_limit):
+    def __init__(self):
         self.id = id
-        self.sheep_move = sheep_move
-        self.x = random.uniform(-init_pos_limit, init_pos_limit)
-        self.y = random.uniform(-init_pos_limit, init_pos_limit)
+        self.sheep_move = 0.5
+        self.x = random.uniform(-10, 10)
+        self.y = random.uniform(-10, 10)
         self.alive = True
 
     def move(self):
         if self.alive:
             direction = random.randint(1, 4)
             if direction == 1:
-                self.y = self.y + self.sheep_move
+                self.y += self.sheep_move
             elif direction == 2:
-                self.x = self.x + self.sheep_move
+                self.x += self.sheep_move
             elif direction == 3:
-                self.y = self.y - self.sheep_move
+                self.y -= self.sheep_move
             elif direction == 4:
-                self.x = self.x - self.sheep_move
+                self.x -= self.sheep_move
 
 
 
 
 class Wolf:
-    def __init__(self, wolf_move, sheep):
+    def __init__(self, sheep):
         self.x = 0.0
         self.y = 0.0
-        self.wolf_move = wolf_move
+        self.wolf_move = 1.0
         self.sheep = sheep
         self.last = None
 
@@ -65,10 +65,10 @@ class Wolf:
                 dist = self.calculate_distance(self.sheep[z])
                 sheep = self.sheep[z]
                 y = False
-            z = z + 1
+            z += 1
         for x in self.sheep:
             if x.alive:
-                if dist >= self.calculate_distance(x):
+                if dist > self.calculate_distance(x):
                     dist = self.calculate_distance(x)
                     sheep = x
         return sheep
@@ -85,36 +85,32 @@ class Wolf:
 
 
 
-# initialize csv lists
+# csv lists
 header = ["Round number", "Number of alive sheep"]
 data = []
 
-# initialize json list
+# json list
 dictionaries = []
 
-# initialize variable
 sheep_list = []
 maximum_number_of_rounds = 50
 the_number_of_sheep = 15
-init_pos_limit = 10.0
-sheep_move = 0.5
-wolf_move = 1.0
 
 # creating wolf and sheeps
 for x in range(the_number_of_sheep):
-    sheep_list.append(Sheep(sheep_move, init_pos_limit))
+    sheep_list.append(Sheep())
     sheep_list[x].id = x
-wo = Wolf(wolf_move, sheep_list)
+wolf = Wolf(sheep_list)
 
 # started values
 number_of_alive = 0
 for sheep in sheep_list:
     if sheep.alive:
-        number_of_alive = number_of_alive + 1
+        number_of_alive += 1
 print("Started values: ")
-print("Wolf position: (", round(wo.x, 3), ",", round(wo.y, 3), ")")
-print("Number of alive sheep: ", number_of_alive)
-print("Id of the closest sheep: ", wo.check_nearest_sheep().id)
+print("Wolf position: (", round(wolf.x, 3), ",", round(wolf.y, 3), ")")
+print("Alive sheep:", number_of_alive)
+print("Id of the closest sheep:", wolf.check_nearest_sheep().id)
 
 # create dictionary for json and adding it to variable
 sheep_position_list = []
@@ -125,7 +121,7 @@ for sheep in sheep_list:
         sheep_position_list.append(None)
 dictionary = {
     "round_no": 0,
-    "wolf_pos": [wo.x, wo.y],
+    "wolf_pos": [wolf.x, wolf.y],
     "sheep_pos": sheep_position_list
 }
 dictionaries.append(dictionary)
@@ -135,28 +131,28 @@ row = [0, number_of_alive]
 data.append(row)
 
 # main code
-number_of_death_sheep_round_ago = 0
+number_of_death_sheep = 0
 for x in range(maximum_number_of_rounds):
-    print("\nRound: ", x + 1)
+    print("\nRound:", x + 1)
     number_of_alive_sheep_before_move = 0
     for sheep in sheep_list:
         if sheep.alive:
-            number_of_alive_sheep_before_move = number_of_alive_sheep_before_move + 1
+            number_of_alive_sheep_before_move += 1
     if number_of_alive_sheep_before_move > 0:
         for sheep in sheep_list:
             sheep.move()
-        wo.move()
+        wolf.move()
         number_of_alive_sheep_after_move = 0
         for sheep in sheep_list:
             if sheep.alive:
-                number_of_alive_sheep_after_move = number_of_alive_sheep_after_move + 1
-        print("Wolf position: (", round(wo.x, 3), ",", round(wo.y, 3), ")")
-        print("Number of alive sheep: ", number_of_alive_sheep_after_move)
+                number_of_alive_sheep_after_move += 1
+        print("Wolf position: (", round(wolf.x, 3), ",", round(wolf.y, 3), ")")
+        print("Alive sheep:", number_of_alive_sheep_after_move)
         if number_of_alive_sheep_after_move > 0:
-            print("Id of the closest sheep: ", wo.check_nearest_sheep().id)
-        if the_number_of_sheep - number_of_alive_sheep_after_move > number_of_death_sheep_round_ago:
-            number_of_death_sheep_round_ago += 1
-            print("Id of sheep caught in last round: ", wo.last.id)
+            print("Id of the closest sheep:", wolf.check_nearest_sheep().id)
+        if the_number_of_sheep - number_of_alive_sheep_after_move > number_of_death_sheep:
+            number_of_death_sheep += 1
+            print("Id of sheep caught in last round:", wolf.last.id)
 
         # create dictionary for json and adding it to variable
         sheep_position_list = []
@@ -167,7 +163,7 @@ for x in range(maximum_number_of_rounds):
                 sheep_position_list.append(None)
         dictionary = {
             "round_no": x + 1,
-            "wolf_pos": [wo.x, wo.y],
+            "wolf_pos": [wolf.x, wolf.y],
             "sheep_pos": sheep_position_list
         }
         dictionaries.append(dictionary)
@@ -180,9 +176,9 @@ for x in range(maximum_number_of_rounds):
         break
 
 # saving to json
-json_object = json.dumps(dictionaries, indent=3)
-with open("pos.json", "w") as outfile:
-    outfile.write(json_object)
+json_object = json.dumps(dictionaries, indent=2)
+with open("pos.json", "w") as jsonfile:
+    jsonfile.write(json_object)
 
 # saving to csv
 with open('alive.csv', 'w', newline='') as file:
